b1eae8d23d06188a8bbbf7f37d3bbe72c8e74e6c
JoonasC <jcoatanea@live.com>
2015-11-24 11:39:30 +0200

    Makin progress #2

--- js/graph.js
+++ js/graph.js
@@ -79,20 +79,22 @@ function gOpen()
 				.enter().append("line")
 				.attr("class", "link")
 				.style("stroke-width", function(d) { return Math.sqrt(d.value); });
-			var node = svg.selectAll(".node")
+			var neuron = svg.selectAll(".neuron")
 				.data(graph.id)
 				.enter().append("circle")
 					.attr("class", "neuron")
 					.attr("r", 10)
 					.style("fill", color(1))
 					.call(force.drag);
+			neuron.append("title")
+				.text(function(d){ return d.name; });
 			force.on("tick", function()
 			{
 				link.attr("x1", function(d) { return d.source.x; })
 					.attr("y1", function(d) { return d.source.y; })
 					.attr("x2", function(d) { return d.target.x; })
 					.attr("y2", function(d) { return d.target.y; });
-				node.attr("cx", function(d) { return d.x; })
+				neuron.attr("cx", function(d) { return d.x; })
 					.attr("cy", function(d) { return d.y; });
 			});
 		});
